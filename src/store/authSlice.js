4e79ad6940384aec07e0d8393c708b0b4f4141c0
HET SHUKLA <SHUKLAHET2704@gmailc.com>
2024-10-11 00:19:03 +0530

    create authSlice.js

---
+++ src/store/authSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    status: false,
+    userData: null
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers:{
+        login: (state, action) => {
+            state.status = true;
+            action.userData = action.payload;
+        },
+
+        logout: (state, action) => {
+            state.status = false;
+            action.userData = null;
+        }
+    }
+});
+
+
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducer;
