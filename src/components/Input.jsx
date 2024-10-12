9bc7be9182fe405a2b8b0e0dab0bcf1f51601983
HET SHUKLA <SHUKLAHET2704@gmailc.com>
2024-10-12 23:33:22 +0530

    Create input.jsx

---
+++ src/components/Input.jsx
@@ -0,0 +1,14 @@
+import React, {useId} from 'react';
+
+const Input = () => {
+
+    const id = useId();
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Input;
