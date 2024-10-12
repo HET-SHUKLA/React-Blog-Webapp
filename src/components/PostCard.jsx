e38c8b6b60d784eba2327d6bb4b728b12639dfa2
HET SHUKLA <SHUKLAHET2704@gmailc.com>
2024-10-13 00:20:26 +0530

    Add PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import appwriteService from '../appwrite/config';
+import service from '../appwrite/config';
 
 const PostCard = ({$id, title, featuredImage}) => {
     return (
-        <div>
-            
-        </div>
+        <Link to={`/post/${$id}`}>
+            <div className='w-full bg-gray-100 rounded-xl p-4'>
+                <div className='w-full justify-center mb-4'>
+                    <img src={service.getFilePreview(featuredImage)} alt={title} className='rounded-xl' />
+                </div>
+                <h2 className='text-xl font-bold'>{title}</h2>
+            </div>
+        </Link>
     );
 }
 
