fa21fedbea769673995c967683e0c920a0ac849b
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-14 01:05:22 +0530

    Add TimeMCE API key

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import { Controller } from 'react-hook-form';
+import conf from '../conf/conf';
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
     return (
@@ -15,6 +16,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 render = {({field: {onChange}}) => (
 
                     <Editor
+                    apiKey={conf.editorAPIkey}
                     initialValue={defaultValue}
                     init={{
                         initialValue: defaultValue,
